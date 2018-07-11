b57fb787f54946f3bf071e6020bf8bf0dba5f81d
gb <670521186@qq.com>
2018-07-11 15:37:52 +0800

    第二次提交

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -4,4 +4,22 @@ package entity;
  * Created by fitch on 2018/7/11.
  */
 public class User {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
